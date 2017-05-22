4f4a5a9d1b290e374635d409e46bd92e9355d9a3
MasterKey13 <Alexandermastryukov@gmail.com>
2017-05-22 21:34:45 +0800

    Initial commit with basic number setup

---
+++ Assets/Scripts/SymbolAssigner.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SymbolAssigner : MonoBehaviour {
+
+	public Sprite[] symbolSprites;
+
+	void Awake()
+	{
+		symbolSprites = Resources.LoadAll<Sprite>("Sprites/symbols");
+	}
+
+	// Use this for initialization
+	void Start () 
+	{
+		this.gameObject.GetComponent<SpriteRenderer>().sprite = symbolSprites[Random.Range(0,15)];
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+}
